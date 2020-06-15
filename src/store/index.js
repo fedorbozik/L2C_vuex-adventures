b06bc494502e158fd3b464f444e62dc06d1667d2
Fedor Božík <fedor.bozik@buckleup.sk>
2020-06-15 10:41:19 +0200

    Cleanup vue store js

--- src/store/index.js
+++ src/store/index.js
@@ -3,29 +3,44 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// STATE
+const state = {
+    score: 15
+}
+
+// MUTATIONS
+const mutations = {
+    INCREMENT(state, value = 1) {
+        state.score += value
+    },
+    DECREMENT(state, value = 1) {
+        state.score -= value
+    }
+}
+
+// ACTIONS
+const actions = {
+    increment(context) {
+        let rand = Math.floor(Math.random() * 100)
+        if (rand < 15) console.error('oh no :(')
+        else context.commit('INCREMENT', rand)
+    }
+}
+
+// GETTERS
+const getters = {
+    scoreButPretty(state) {
+        return state.score > 100 ? `YAY ${state.score} YAY` : state.score
+    }
+}
+
+// MODULES
+const modules = {}
+
 export default new Vuex.Store({
-    state: {
-        score: 15
-    },
-    mutations: {
-        INCREMENT(state, value = 1) {
-            state.score += value
-        },
-        DECREMENT(state, value = 1) {
-            state.score -= value
-        }
-    },
-    actions: {
-        increment(context) {
-            let rand = Math.floor(Math.random() * 100)
-            if (rand < 15) console.error('oh no :(')
-            else context.commit('INCREMENT', rand)
-        }
-    },
-    getters: {
-        scoreButPretty(state) {
-            return state.score > 100 ? `YAY ${state.score} YAY` : state.score
-        }
-    },
-    modules: {}
+    state,
+    mutations,
+    actions,
+    getters,
+    modules
 })
